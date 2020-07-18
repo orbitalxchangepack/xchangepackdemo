7915bcd0df2912f8d8cd4b8780cb2891d0370e7f
charmainehly <65525578+charmainehly@users.noreply.github.com>
2020-07-18 23:59:25 +0800

    minor change

--- server/node_react_forum.js
+++ server/node_react_forum.js
@@ -28,7 +28,5 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "client/public/index.html"));
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log("Server is up on port" + port);
-});
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
