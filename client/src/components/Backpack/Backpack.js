0ef78b585ec6d60b17cb53ed8601e307d28b460f
charmainehly <65525578+charmainehly@users.noreply.github.com>
2020-07-25 14:44:18 +0800

    smallchange

--- client/src/components/Backpack/Backpack.js
+++ client/src/components/Backpack/Backpack.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Grid, Container } from "semantic-ui-react";
+import { Grid } from "semantic-ui-react";
 import Filter from "./Filter";
 import "./Backpack.css";
 
