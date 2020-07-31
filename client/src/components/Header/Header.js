bd65fa9d5fde7e0d4a8d8559e6a207298721f24c
charmainehly <65525578+charmainehly@users.noreply.github.com>
2020-07-31 21:10:58 +0800

    navbar logos

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -16,26 +16,57 @@ export default class header extends Component {
           <ul>
             <li className="link__wrapper">
               <NavLink exact activeClassName="active" to="/">
+                <img
+                  className="navbar_logo"
+                  src={process.env.PUBLIC_URL + "/home_logo.png"}
+                  alt="home logo"
+                  float="left"
+                ></img>
                 <div className="navbar">HOME</div>
               </NavLink>
             </li>
             <li className="link__wrapper">
               <NavLink activeClassName="active" to="/SchoolReviews">
+                <img
+                  className="navbar_logo"
+                  src={process.env.PUBLIC_URL + "/schoolreviews_logo.png"}
+                  alt="home logo"
+                  float="left"
+                ></img>
+
                 <div className="navbar">School Reviews</div>
               </NavLink>
             </li>
             <li className="link__wrapper">
               <NavLink activeClassName="active" to="/MappingPlanner">
+                <img
+                  className="navbar_logo"
+                  src={process.env.PUBLIC_URL + "/mappingplanner_logo.png"}
+                  alt="home logo"
+                  float="left"
+                ></img>
                 <div className="navbar">Mapping Planner</div>
               </NavLink>
             </li>
             <li className="link__wrapper">
               <NavLink activeClassName="active" to="/Backpack">
+                <img
+                  className="navbar_logo"
+                  src={process.env.PUBLIC_URL + "/backpack_logo.png"}
+                  alt="home logo"
+                  float="left"
+                ></img>
                 <div className="navbar">Backpack</div>
               </NavLink>
             </li>
             <li className="link__wrapper">
               <NavLink activeClassName="active" to="/Ask">
+                <img
+                  className="navbar_logo"
+                  src={process.env.PUBLIC_URL + "/askforum_logo.png"}
+                  alt="home logo"
+                  float="left"
+                ></img>
                 <div className="navbar">Ask!</div>
               </NavLink>
             </li>
